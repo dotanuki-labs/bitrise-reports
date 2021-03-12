cb7f025e0aefe644f9afb1ea4fef7d7dfe8ee89e
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-03-12 11:56:59 +0100

    Added implementation to fetch information from Bitrise API (#4)
    * Added request and pytest-httpserver
    
    * Implemented raw data converters
    
    * Implemented BitriseAPI integration with paging support
    
    * Implemented Bitrise and integration tests

---
+++ bitrise_reports/bitrise.py
@@ -0,0 +1,108 @@
+# bitrise.py
+
+from .errors import ErrorCause, BitriseIntegrationError
+from .models import BitriseBuild, BitriseProject, BuildStack
+from .models import BuildMachine, BuildMinutes, BitriseWorkflow, MachineSize
+
+from datetime import datetime
+from math import ceil
+
+import logging
+import requests
+
+BITRISE_API_URL = 'https://api.bitrise.io/v0.1'
+FIRST_PAGE = 'first-page'
+NO_MORE_PAGES = 'no-more-pages'
+
+
+class Bitrise(object):
+
+    def __init__(self, api_token):
+        self.api = BitriseApiFetcher(api_token)
+        self.converter = RawDataConverter()
+
+    def available_projects(self):
+        endpoint = f"{BITRISE_API_URL}/apps"
+        raw_data = self.api.get(endpoint)
+        return self.converter.projects_from(raw_data)
+
+    def builds_for_project(self, project):
+        endpoint = f"{BITRISE_API_URL}/apps/{project.slug}/builds"
+        raw_data = self.api.get(endpoint)
+        return self.converter.builds_from(raw_data, project)
+
+
+class BitriseApiFetcher(object):
+
+    def __init__(self, api_token):
+        self.auth = {'Authorization': api_token}
+
+    def get(self, endpoint):
+        results = []
+        next = FIRST_PAGE
+
+        while next != NO_MORE_PAGES:
+            params = None if next == FIRST_PAGE else {'next': next}
+            fetched, next_page = self.__get_page(endpoint, params)
+            results.extend(fetched)
+            next = next_page
+
+        return results
+
+    def __get_page(self, endpoint, args=None):
+        response = requests.get(endpoint, headers=self.auth, params=args)
+
+        if response.status_code == requests.codes.ok:
+            data = response.json()['data']
+            paging = response.json()['paging']
+            next = paging['next'] if 'next' in paging.keys() else NO_MORE_PAGES
+            return data, next
+        else:
+            raise BitriseIntegrationError(ErrorCause.Http)
+
+
+class RawDataConverter(object):
+
+    def projects_from(self, json):
+        try:
+            return [BitriseProject(item['title'], item['slug']) for item in json]
+        except:
+            logging.exception("An exception occurred")
+            logging.error("Could not parse/convert information from projects")
+            raise BitriseIntegrationError(ErrorCause.ApiDataConversion)
+
+    def builds_from(self, json, project):
+        try:
+            finished_builds = list(filter(lambda raw: raw['finished_at'] is not None, json))
+            return [self.build_from(item, project) for item in finished_builds]
+        except Exception as e:
+            print(e)
+            logging.exception("An exception occurred")
+            logging.error("Could not parse/convert information from builds")
+            raise BitriseIntegrationError(ErrorCause.ApiDataConversion)
+
+    def build_from(self, json, project):
+        machine = self.machine_from(json['machine_type_id'], json['stack_identifier'])
+        workflow = self.workflow_from(json['triggered_workflow'], project)
+        minutes = self.minutes_from(json['triggered_at'], json['started_on_worker_at'], json['finished_at'])
+        return BitriseBuild(project, machine, workflow, minutes)
+
+    def machine_from(self, machine_type_id, stack_identifier):
+        size = MachineSize(machine_type_id)
+        stack = BuildStack('macos' if 'osx' in stack_identifier else 'linux')
+        return BuildMachine(f"{stack.value}.{size.value}", size, stack)
+
+    def workflow_from(self, triggered_workflow, bitrise_project):
+        return BitriseWorkflow(triggered_workflow)
+
+    def minutes_from(self, triggered_at, started_at, finished_at):
+        triggered = datetime.fromisoformat(triggered_at.replace('Z',''))
+        started = datetime.fromisoformat(triggered_at.replace('Z',''))
+        finished = datetime.fromisoformat(finished_at.replace('Z',''))
+        queued = self.__aproximate_minutes(started - triggered)
+        building = self.__aproximate_minutes(finished - started)
+        total = self.__aproximate_minutes(finished - triggered)
+        return BuildMinutes(queued, building, total)
+
+    def __aproximate_minutes(self, diff):
+        return ceil((diff.days * 24 * 60) + (diff.seconds / 60))
---
+++ tests/test_data_converter.py
@@ -0,0 +1,74 @@
+# test_data_converter.py
+
+from bitrise_reports.models import BitriseProject, BitriseBuild, BuildMinutes
+from bitrise_reports.models import BuildStack, MachineSize, BuildMachine, BitriseWorkflow
+from bitrise_reports.bitrise import RawDataConverter
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "machine_type_id,stack_id,id,size,stack", [
+        ('elite', 'osx-xcode-12.0.x', 'macos.elite', MachineSize.medium, BuildStack.osx),
+        ('elite-xl', 'android-docker-linux', 'linux.elite-xl', MachineSize.large, BuildStack.linux),
+        ('standard', 'android-docker-linux', 'linux.standard', MachineSize.small, BuildStack.linux)
+    ]
+)
+def test_convert_machines(machine_type_id, stack_id, id, size, stack):
+    converter = RawDataConverter()
+
+    converted = converter.machine_from(machine_type_id, stack_id)
+
+    expected = BuildMachine(id, size, stack)
+    assert converted == expected
+
+
+def test_convert_workflow():
+    converter = RawDataConverter()
+    project = BitriseProject('my-project', 'c92318adbbe2755f')
+    workflow = 'pull-request'
+
+    converted = converter.workflow_from(workflow, project)
+
+    expected = BitriseWorkflow('pull-request')
+    assert converted == expected
+
+
+def test_convert_minutes():
+    converter = RawDataConverter()
+
+    triggered_at = '2021-03-01T09:05:52Z'
+    started_at = '2021-03-01T09:06:42Z'
+    finished_at = '2021-03-01T09:16:45Z'
+
+    converted = converter.minutes_from(triggered_at, started_at, finished_at)
+
+    queued = 0
+    building = 11
+    total = 11
+    assert converted == BuildMinutes(queued, building, total)
+
+
+def convert_bitrise_build():
+    converter = RawDataConverter()
+    android = BitriseProject('my-project', 'c92318adbbe2755f')
+
+    build = {
+        'triggered_at':'2021-03-01T09:05:22Z',
+        'started_on_worker_at':'2021-03-01T09:06:33Z',
+        'finished_at':'2021-03-01T09:35:45Z',
+        'machine_type_id':'elite-xl',
+        'stack_identifier':'android-docker-linux',
+        'triggered_workflow':'espresso-tests'
+    }
+
+    converted = converter.convert_build(build, android)
+
+    expected = BitriseBuild(
+        project=android,
+        machine=BuildMachine(MachineSize.medium, BuildStack.osx),
+        workflow=BitriseWorkflow('espresso-tests'),
+        minutes=BuildMinutes(1, 35, 36)
+    )
+
+    assert converted == expected
