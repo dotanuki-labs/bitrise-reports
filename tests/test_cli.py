aae462eb3f9aae27d000a56137340339ca0dec34
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-03-19 18:45:17 +0100

    Tests for CLI args validation

---
+++ tests/test_cli.py
@@ -0,0 +1,37 @@
+# test_cli.py
+
+from bitrise_reports import cli
+from bitrise_reports.errors import CLIArgumentsError
+from bitrise_reports.models import EvaluationCriteria
+
+import pytest
+
+
+def test_extract_criteria():
+
+    # Given
+    app = 'android-flagship'
+    starting = '2021-03-01'
+    ending = '2021-03-31'
+
+    # When
+    criteria = cli.parse_criteria(app, starting, ending)
+
+    # Then
+    expected = EvaluationCriteria(app, 1614553200, 1617141600)
+    assert criteria == expected
+
+
+def test_fail_when_missing_app():
+
+    with pytest.raises(Exception) as error:
+        # Given
+        app = ''
+        starting = '2021-03-01'
+        ending = '2021-03-31'
+
+        # When
+        cli.parse_criteria(app, starting, ending)
+
+        # Then
+        assert error.cause is CLIArgumentsError
