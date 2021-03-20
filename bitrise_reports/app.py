c5b328f19d032adfd578da01719453ddc13cd936
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-03-20 23:24:54 +0100

    Getting Bitrise API token from CLI args

--- bitrise_reports/app.py
+++ bitrise_reports/app.py
@@ -8,18 +8,20 @@ import logging
 import sys
 
 APP_HELP = "The title of your app in Bitrise"
-START_HELP = "Starting date to drive the analysis (YYYY-MM-DD)"
 ENDING_HELP = "Ending date to drive the analysis (YYYY-MM-DD)"
+START_HELP = "Starting date to drive the analysis (YYYY-MM-DD)"
+TOKEN_HELP = "A Personal Access Token for Bitrise API"
 
 logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger(__name__)
 
 
 @click.command()
+@click.option("--token", required=True, help=TOKEN_HELP)
 @click.option("--app", required=True, help=APP_HELP)
 @click.option("--starting", required=True, help=START_HELP)
 @click.option("--ending", required=True, help=ENDING_HELP)
-def launch(app, starting, ending):
+def launch(token, app, starting, ending):
     try:
         criteria = cli.parse_criteria(app, starting, ending)
         LOG.info(f"{criteria}")
