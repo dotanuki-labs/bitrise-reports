348d8dc8c21b791332144c58fdce136f766f0bed
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-03-20 23:30:19 +0100

    Fixing Bandit warning

--- bitrise_reports/app.py
+++ bitrise_reports/app.py
@@ -10,14 +10,14 @@ import sys
 APP_HELP = "The title of your app in Bitrise"
 ENDING_HELP = "Ending date to drive the analysis (YYYY-MM-DD)"
 START_HELP = "Starting date to drive the analysis (YYYY-MM-DD)"
-TOKEN_HELP = "A Personal Access Token for Bitrise API"
+BITRISE_PAT_HELP = "A Personal Access Token (PAT) for Bitrise API"
 
 logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger(__name__)
 
 
 @click.command()
-@click.option("--token", required=True, help=TOKEN_HELP)
+@click.option("--token", required=True, help=BITRISE_PAT_HELP)
 @click.option("--app", required=True, help=APP_HELP)
 @click.option("--starting", required=True, help=START_HELP)
 @click.option("--ending", required=True, help=ENDING_HELP)
