19134b3b666ad6f876eb73e25cbcb0b4ab43c5ce
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-03-21 13:35:57 +0100

    Normalise terminology around Bitrise apps

--- bitrise_reports/bitrise.py
+++ bitrise_reports/bitrise.py
@@ -19,15 +19,15 @@ class ProjectSlugFinder(object):
     def __init__(self, bitrise):
         self.bitrise = bitrise
 
-    def find(self, app_name):
-        apps = self.bitrise.available_projects()
-        target = next(filter(lambda app: app.id == app_name, apps), None)
+    def find(self, name):
+        projects = self.bitrise.available_projects()
+        target = next(filter(lambda project: project.id == name, projects), None)
 
         if not target:
-            logging.error("Cannot locate app with name {app_name}")
-            logging.error("Available apps for this user")
-            logging.error(apps)
-            message = f"{app_name} not available in the projects this user has access"
+            logging.error("Cannot locate projects with name {name}")
+            logging.error("Available projects for this user")
+            logging.error(projects)
+            message = f"{name} not available in the projects this user has access"
             raise BitriseMiddlewareError(message)
 
         return target
