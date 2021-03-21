9440545809586aefa9dbadd37136a14b8ee697fa
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-03-21 23:44:37 +0100

    Defined BuildsAnalyser on middleware

--- bitrise_reports/middleware.py
+++ bitrise_reports/middleware.py
@@ -4,7 +4,8 @@ from bitrise_reports.errors import BitriseMiddlewareError
 
 import logging
 
-class ProjectSlugFinder(object):
+
+class ProjectFinder(object):
     def __init__(self, bitrise):
         self.bitrise = bitrise
 
@@ -13,10 +14,32 @@ class ProjectSlugFinder(object):
         target = next(filter(lambda project: project.id == name, projects), None)
 
         if not target:
-            logging.error("Cannot locate projects with name {name}")
+            logging.error("Cannot locate an app with name: {name}")
             logging.error("Available projects for this user")
             logging.error(projects)
-            message = f"{name} not available in the projects this user has access"
-            raise BitriseMiddlewareError(message)
+            cause = f"{name} not present among projects you have access"
+            raise BitriseMiddlewareError(cause)
 
         return target
+
+
+class BuildsAnalyser(object):
+    def __init__(self, bitrise, cruncher):
+        self.bitrise = bitrise
+        self.cruncher = cruncher
+
+    def analyse(self, project, starting, ending):
+        builds = self.bitrise.builds_for_project(project, starting, ending)
+
+        if not builds:
+            logging.error(f"Cannot locate find builds for project : {project.id}")
+            cause = f"{project.id} has no builds yet"
+            raise BitriseMiddlewareError(cause)
+
+        breakdowns = [
+            self.cruncher.breakdown_per_project(builds),
+            self.cruncher.breakdown_per_machine(builds),
+            self.cruncher.breakdown_per_workflow(builds),
+        ]
+
+        return breakdowns
