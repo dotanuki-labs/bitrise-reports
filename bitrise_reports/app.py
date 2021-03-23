cf3505009d67867917b76c5b7783a950e648a758
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-03-23 11:02:55 +0100

    Wired up full app using stdout reporter

--- bitrise_reports/app.py
+++ bitrise_reports/app.py
@@ -1,5 +1,7 @@
 # app.py
 
+from dataclasses import astuple
+
 
 class Application(object):
     def __init__(self, finder, analyser, reporter, criteria):
@@ -9,7 +11,12 @@ class Application(object):
         self.execution_criteria = criteria
 
     def execute(self):
-        bitrise_app, starting, ending = self.criteria
+        bitrise_app, starting, ending = astuple(self.execution_criteria)
+
+        self.results_reporter.started(bitrise_app)
+
         project = self.project_finder.find(bitrise_app)
+        self.results_reporter.feedback(project)
+
         breakdowns = self.project_analyser.analyse(project, starting, ending)
-        self.results_reporter.report(breakdowns, starting, ending)
+        self.results_reporter.report(breakdowns)
