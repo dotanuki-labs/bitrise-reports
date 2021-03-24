3788603fc4f6faa5db5b7b51ff7cb536d3c20306
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-03-24 12:57:37 +0100

    Added flag for velocity option

--- bitrise_reports/entrypoint.py
+++ bitrise_reports/entrypoint.py
@@ -12,6 +12,7 @@ APP_HELP = "The title of your app in Bitrise"
 ENDING_HELP = "Ending date to drive the analysis (YYYY-MM-DD)"
 START_HELP = "Starting date to drive the analysis (YYYY-MM-DD)"
 BITRISE_PAT_HELP = "A Personal Access Token (PAT) for Bitrise API"
+VELOCITY_HELP = "Estimate Bitrise Velocity credits related to builds"
 OUTPUT_HELP = "The style of report you want (stdout | json | excel). Defaults to stdout"
 
 logging.basicConfig(level=logging.INFO)
@@ -22,10 +23,11 @@ logging.basicConfig(level=logging.INFO)
 @click.option("--app", required=True, help=APP_HELP)
 @click.option("--starting", required=True, help=START_HELP)
 @click.option("--ending", required=True, help=ENDING_HELP)
+@click.option("--velocity", is_flag=True, default=False, help=VELOCITY_HELP)
 @click.option("--report", required=False, default="stdout", help=OUTPUT_HELP)
-def launch(token, app, starting, ending, report):
+def launch(token, app, starting, ending, velocity, report):
     try:
-        app = di.inject(token, app, starting, ending, report)
+        app = di.inject(token, app, starting, ending, velocity, report)
         app.execute()
         sys.exit(0)
     except Exception as error:
